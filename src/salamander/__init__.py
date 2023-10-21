19af3e8cec7a05a94c8a4448edd2ecf41d0e329d
BeGeiger <benedikt-geiger@gmx.de>
2023-10-21 12:40:08 -0400

    bump version

--- src/salamander/__init__.py
+++ src/salamander/__init__.py
@@ -7,5 +7,5 @@ from .nmf_framework.klnmf import KLNMF
 from .nmf_framework.multimodal_corrnmf import MultimodalCorrNMF
 from .nmf_framework.mvnmf import MvNMF
 
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 __all__ = ["CorrNMFDet", "KLNMF", "MvNMF", "MultimodalCorrNMF"]
