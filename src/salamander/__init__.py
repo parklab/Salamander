78ac2788782161457acd6b58959305fa5c5087b7
BeGeiger <benedikt-geiger@gmx.de>
2024-03-28 14:55:02 -0400

    remove files
    Plan:
    - make Salamander compatible with anndata
    - reimplement NMF models one at a time
    
    Remove all algorithms except KL-NMF. The goal is to avoid having a broken software along the refactorization.

--- src/salamander/__init__.py
+++ src/salamander/__init__.py
@@ -2,14 +2,11 @@
 Salamander: a non-negative matrix factorization framework for signature analysis
 ================================================================================
 """
-from .nmf_framework.corrnmf_det import CorrNMFDet
 from .nmf_framework.klnmf import KLNMF
-from .nmf_framework.multimodal_corrnmf import MultimodalCorrNMF
-from .nmf_framework.mvnmf import MvNMF
 from .plot import set_salamander_style
 
 __version__ = "0.3.2"
-__all__ = ["CorrNMFDet", "KLNMF", "MvNMF", "MultimodalCorrNMF"]
+__all__ = ["KLNMF"]
 
 
 set_salamander_style()
