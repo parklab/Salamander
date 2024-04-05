addd0a0dcad80ef2990a9ee7b10d952bb86b24c3
BeGeiger <benedikt-geiger@gmx.de>
2024-04-05 01:44:33 -0400

    fix init
    KLNMF not refactored yet

--- src/salamander/__init__.py
+++ src/salamander/__init__.py
@@ -2,11 +2,11 @@
 Salamander: a non-negative matrix factorization framework for signature analysis
 ================================================================================
 """
-from .nmf_framework.klnmf import KLNMF
+# from .nmf_framework.klnmf import KLNMF
 from .plot import set_salamander_style
 
 __version__ = "0.3.2"
-__all__ = ["KLNMF"]
+# __all__ = ["KLNMF"]
 
 
 set_salamander_style()
