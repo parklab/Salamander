fff3c81384e867fa722acd3cff726a3a140ba6db
BeGeiger <benedikt-geiger@gmx.de>
2024-04-25 04:27:09 -0400

    improve init

--- src/salamander/__init__.py
+++ src/salamander/__init__.py
@@ -3,14 +3,10 @@ Salamander: a non-negative matrix factorization framework for signature analysis
 ================================================================================
 """
 
+from . import models
 from . import plot as pl
 from . import tools as tl
-from .models.corrnmf_det import CorrNMFDet
-from .models.klnmf import KLNMF
-from .models.mvnmf import MvNMF
 
 __version__ = "0.3.2"
-__all__ = ["CorrNMFDet", "KLNMF", "MvNMF"]
-
 
 pl.set_salamander_style()
--- src/salamander/models/__init__.py
+++ src/salamander/models/__init__.py
@@ -1,1 +1,7 @@
-""
+"""
+Multiple NMF variants
+"""
+
+from .corrnmf_det import CorrNMFDet
+from .klnmf import KLNMF
+from .mvnmf import MvNMF
